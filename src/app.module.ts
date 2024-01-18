50cf803a9ae12e5d0d636c3e392e0f51105378c1
Jesus Marenco <jesusdario10@hotmail.com>
2024-01-18 18:19:40 -0500

    configuration db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { QuotationModule } from './quotation/quotation.module';
 import { SolutionsModule } from './solutions/solutions.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot('mongodb://localhost:27018/sici'),
     QuotationModule,
     SolutionsModule,
   ],
