6f91340bb1a50cb60e5e9641f1e0f9094816691d
Jesus Marenco <jesusdario10@hotmail.com>
2024-01-18 17:44:05 -0500

    config prefix api un main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { QuotationModule } from './quotation/quotation.module';
+import { SolutionsModule } from './solutions/solutions.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    QuotationModule,
+    SolutionsModule,
   ],
   controllers: [],
   providers: [],
